46f5880861813ced91a4bd10288b35f225a001b9
Helton Ricardo <50843386+heltonr13@users.noreply.github.com>
2020-12-28 14:45:35 -0300

    Create format80.py

---
+++ files/title-format80/format80.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+while True:
+   e = input('Title: ').upper()
+   b = 80 - int(input('Indentation: '))
+   t = len(e) + 6
+   i = int((b - t) / 2)
+   if b % 2 and not len(e) % 2: i += 1
+   f = b - t - i
+   msg = f'/*{i*"*"} {e} {f*"*"}*/'
+   df = pd.DataFrame([msg])
+   df.to_clipboard(index = False, header = False)
+   print(' > Copied to the clipboard!\n')
