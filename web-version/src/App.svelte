6c1371c63f339de2f65b2572269c2ff1d12ddd9d
heltonricardo <50843386+heltonricardo@users.noreply.github.com>
2021-06-29 15:27:31 -0300

    logica e mais estilo

--- web-version/src/App.svelte
+++ web-version/src/App.svelte
@@ -1,5 +1,22 @@
 <script>
-  function ok() {}
+  let campo = "";
+  let indentacao = 2;
+  let resultado;
+
+  async function ok() {
+    const titulo = campo.trim().toUpperCase();
+    const area = 80 - indentacao;
+    const texto = titulo.length + 6;
+    let inicio = parseInt((area - texto) / 2);
+    if (area % 2 && !titulo.length % 2) ++inicio;
+    const fim = area - texto - inicio;
+    const msg = `/*${"*".repeat(inicio)} ${titulo} ${"*".repeat(fim)}*/`;
+    resultado.value = msg;
+    await resultado.select();
+    await resultado.setSelectionRange(0, 99999);
+    await document.execCommand("copy");
+    campo = "";
+  }
 </script>
 
 <style>
@@ -17,8 +34,8 @@
   }
 
   #conteudo {
-    width: 25%;
-    height: 60%;
+    width: 30rem;
+    height: 30rem;
     background-color: #eee;
     border-radius: 0.5rem;
     border-width: 1rem;
@@ -37,22 +54,60 @@
     font-size: 1.2rem;
   }
 
-  input {
+  #entrada {
     width: 90%;
     font-size: 1.5rem;
     margin: 2rem 0;
+    text-align: center;
+  }
+
+  #indent {
+    display: flex;
+    align-items: center;
+    width: 45%;
+    justify-content: space-between;
+  }
+
+  #controle {
+    display: flex;
+    align-items: center;
+    width: 90%;
+    justify-content: space-between;
+    height: 1rem;
+  }
+
+  #indentacao {
+    width: 5rem;
+    font-size: 1.5rem;
+    margin: 2rem 0;
+    text-align: center;
   }
 
   button {
     width: 3rem;
     height: 2rem;
   }
+
+  #sumiu {
+    position: fixed;
+    top: 100%;
+  }
 </style>
 
 <div id="corpo">
   <div id="conteudo">
     <h3>Insira o título abaixo e pressione OK:</h3>
-    <input type="text" />
-    <button on:click={ok}>OK</button>
+
+    <input id="entrada" type="text" bind:value={campo} />
+
+    <div id="controle">
+      <div id="indent">
+        <label for="indentacao">Indentação:</label>
+        <input id="indentacao" type="number" bind:value={indentacao} />
+      </div>
+      <button on:click={ok}>OK</button>
+    </div>
   </div>
 </div>
+
+<input type="text" id="sumiu" bind:this={resultado} />
